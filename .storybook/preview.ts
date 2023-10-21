99e1d1bdf984c2137c41c4cd491a6a7f518eccd5
Orkun Tumer <orkun.tumer@fireflyon.com>
2023-10-21 21:41:55 +0300

    fix: github actions with upload download artifact

--- .storybook/preview.ts
+++ .storybook/preview.ts
@@ -6,12 +6,7 @@ import { light } from "../src/theme/light";
 const preview: Preview = {
   parameters: {
     actions: { argTypesRegex: "^on[A-Z].*" },
-    controls: {
-      matchers: {
-        color: /(background|color)$/i,
-        date: /Date$/i,
-      },
-    },
+    controls: { matchers: { color: /(background|color)$/i, date: /Date$/i } },
     backgrounds: {
       default: "light",
       values: [
